617ba2f16c36b7dec4d2d4605963a225c80553ec
Sobrah <sbhnrhmsl@outlook.com>
2023-12-26 13:21:17 +0330

    Add Cat

--- graphic.c
+++ graphic.c
@@ -1,24 +1,41 @@
 #include <raylib.h>
 
+
 // Constant Global Variables
-#define SCREEN_SIZE 720
+#define SCREEN_SIZE 650
 #define FRAME_PER_SECOND 30
 #define TABLE_SIZE 15
 
+
 // Prototypes
 void drawBoard(Color, Color);
+
 
 int main(void) {
     // Initialize Window
     InitWindow(SCREEN_SIZE, SCREEN_SIZE, "Mr. Clobber's Backyard");
     SetTargetFPS(FRAME_PER_SECOND);
 
-    while (!WindowShouldClose()) {
-        drawBoard(BROWN, DARKBROWN);
-    }
+    // Drawing Board Background
+    drawBoard(BROWN, DARKBROWN);
+    
+    // Add Cat Image to the Board
+    Image catImage = LoadImage("Images/Yellow Cat.png");
+    ImageResize(&catImage, SCREEN_SIZE / TABLE_SIZE, SCREEN_SIZE / TABLE_SIZE);  
+    Texture2D catTexture = LoadTextureFromImage(catImage);
+    UnloadImage(catImage);
 
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+            DrawTexture(catTexture, SCREEN_SIZE / TABLE_SIZE, SCREEN_SIZE / TABLE_SIZE, WHITE);
+        EndDrawing();
+    }
+    
+    // Unload Texture From Memory
+    UnloadTexture(catTexture);
     CloseWindow();   
 }
+
 
 void drawBoard(Color backgroundColor, Color lineColor) {
     int sideSize = SCREEN_SIZE / TABLE_SIZE;
