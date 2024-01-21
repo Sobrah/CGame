8d1322084248888c798de1196f2f16fd3f034972
ssaberifar <s.o.r.o.u.s.h.s.a.b.e.r.i.f.a.r@gmail.com>
2024-01-21 14:24:04 +0330

    Set Cats Texture

--- dice.c
+++ dice.c
@@ -1,6 +1,7 @@
 #include "graphic.c"
 
 #define diceLength (sizeof(DICE_PATHS) / sizeof(char *))
+#define catlength (sizeof(CAT_PATHS) / sizeof(char *))
 
 char *DICE_PATHS[] = {
     "Images/Dice/Craps.svg",
@@ -12,7 +13,15 @@ char *DICE_PATHS[] = {
     "Images/Dice/Dice Six.svg"
 };
 
+char *CAT_PATHS[] = {
+    "Images/Blue Cat.svg",
+    "Images/Green Cat.svg",
+    "Images/Purple Cat.svg",
+    "Images/Yellow Cat.svg"
+};
+
 Texture DICE_TEXTURES[diceLength];
+Texture CAT_TEXTURES[catlength];
 
 void DiceScreen(void) {
     
@@ -20,18 +29,44 @@ void DiceScreen(void) {
     for (int i = 0; i < diceLength; i++) {
         Image itemImage = LoadImageSvg(
             DICE_PATHS[i],
-            3 * CELL_SIZE,
-            3 * CELL_SIZE
+            2 * CELL_SIZE,
+            2 * CELL_SIZE
         );
         DICE_TEXTURES[i] = LoadTextureFromImage(itemImage);
         UnloadImage(itemImage);
     }
+    for(int i = 0; i < catlength ; i++){
+        Image itemImage = LoadImageSvg(
+            CAT_PATHS[i],
+            2 * CELL_SIZE,
+            2 * CELL_SIZE
+        );
+        CAT_TEXTURES[i] = LoadTextureFromImage(itemImage);
+        UnloadImage(itemImage);
+    }
+
+    // Set Textures Cordination
+    int x = 60;
+    int y = 2 *CELL_SIZE;
+
+    int dicenumbers[catlength];
+
+    
+
 
     while (!WindowShouldClose()) {
         BeginDrawing();
             ClearBackground(BROWN);
 
-            DrawTexture(DICE_TEXTURES[0], 500, 500, WHITE);
+        
+        // Draw Cat Textures
+        for (int i = 0 ; i < catlength ; x += 60 + 2* CELL_SIZE, i++)
+        {
+            DrawTexture(CAT_TEXTURES[i], x ,y , WHITE);
+        }
+        
+            DrawTexture(DICE_TEXTURES[0], 362 , 550, WHITE);
+
         EndDrawing();
     }
 }
