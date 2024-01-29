484aa25c062e0f10ae7ae28f36e193828dbf3e82
Sobrah <sbhnrhmsl@outlook.com>
2024-01-29 22:50:20 +0330

    Fixing Dice

--- dice.c
+++ dice.c
@@ -1,141 +1,126 @@
-#include "graphic.c"
-#include <stdio.h>
+#include "screen.c"
 
-#define TEXTURE_SIZE (3 * CELL_SIZE)
-#define CAT_LENGTH (sizeof(CAT_PATHS) / sizeof(char *))
-#define DICE_LENGTH (sizeof(DICE_PATHS) / sizeof(char *))
 
-char *DICE_PATHS[] = {
-    "Images/Dice/Craps.svg",
-    "Images/Dice/Dice One.svg",
-    "Images/Dice/Dice Two.svg",
-    "Images/Dice/Dice Three.svg",
-    "Images/Dice/Dice Four.svg",
-    "Images/Dice/Dice Five.svg",
-    "Images/Dice/Dice Six.svg"
+#define TEXTURE_SIZE (PROPERTY_LENGTH * CELL_SIZE)
+#define CAT_LENGTH (sizeof(CAT_PATURES) / sizeof(Pature))
+#define DICE_LENGTH (sizeof(DICE_PATURES) / sizeof(Pature))
+
+
+Pature DICE_PATURES[] = {
+    {"Craps.svg"},
+    {"One.svg"},
+    {"Two.svg"},
+    {"Three.svg"},
+    {"Four.svg"},
+    {"Five.svg"},
+   { "Six.svg"}
 };
 
-char *CAT_PATHS[] = {
-    "Images/Blue Cat.svg",
-    "Images/Green Cat.svg",
-    "Images/Purple Cat.svg",
-    "Images/Yellow Cat.svg"
+Pature CAT_PATURES[] = {
+    {"Purple Cat.svg"},
+    {"Green Cat.svg"},
+    {"Yellow Cat.svg"},
+    {"Blue Cat.svg"}
 };
 
-Texture DICE_TEXTURES[DICE_LENGTH];
-Texture CAT_TEXTURES[CAT_LENGTH];
+int Dice[CAT_LENGTH] = {4, 4, 4, 4};
 
+// Check Repetitive Dice
+int CheckRepeat(void) {
+    for (int i = 0; i < CAT_LENGTH; i++) {
+        for (int j = i + 1; j < CAT_LENGTH; j++) {
+            if (Dice[i] == Dice[j]) return i;
+        }
+    }
+    return CAT_LENGTH;
+}
 
-// Find Same DiceNumber
-int SameNumber(int diceNumbers[CAT_LENGTH], int cat_lenght , int *i , int *j){
+void LoadPatures(char *basePath, Pature *Patures, int n) {
+    for (int i = 0; i < n; i++) {
+        const char *path[] = {basePath, Patures[i].path};
+        
+        Image picture = LoadImageSvg(
+            TextJoin(path, sizeof(path) / sizeof(char *), ""),
+            TEXTURE_SIZE,
+            TEXTURE_SIZE
+        );
 
-            int m ,n ;
-            for(m = 0 ; m < cat_lenght ; m++)
-                for(n = 0 ; n < cat_lenght ; n++){
-                    if(n != m && diceNumbers[m] == diceNumbers[n]){ *i = m ; *j = n; return 1;} 
-                }
+        Patures[i].texture = LoadTextureFromImage(picture);
 
-            return 0;
-
+        UnloadImage(picture);
+    }
 }
 
 void DiceScreen(void) {
-    
+
     // Load Dice Textures
-    for (int i = 0; i < DICE_LENGTH; i++) {
-        Image itemImage = LoadImageSvg(
-            DICE_PATHS[i], TEXTURE_SIZE, TEXTURE_SIZE
-        );
-        DICE_TEXTURES[i] = LoadTextureFromImage(itemImage);
-        UnloadImage(itemImage);
-    }
+    LoadPatures("Images/Dice/", DICE_PATURES, DICE_LENGTH);
     
-    // Cat Textures
-    for(int i = 0; i < CAT_LENGTH; i++){
-        Image itemImage = LoadImageSvg(
-            CAT_PATHS[i], TEXTURE_SIZE, TEXTURE_SIZE
-        );
-        CAT_TEXTURES[i] = LoadTextureFromImage(itemImage);
-        UnloadImage(itemImage);
-    }
+    // Load Cat Textures
+    LoadPatures("Images/Board/", CAT_PATURES, CAT_LENGTH);
 
-
-    int diceNumbers[CAT_LENGTH];
-    int index = 0 ;
-    int i  = 0 , j = 0 ;
+    // Current User Index
+    int index = 4;
 
     while (!WindowShouldClose()) {
         
         // Set Textures Coordination
-        int xspace = (WINDOW_WIDTH - CAT_LENGTH * TEXTURE_SIZE) / (CAT_LENGTH+1);
+        int xspace = (WINDOW_WIDTH - CAT_LENGTH * TEXTURE_SIZE) / (CAT_LENGTH + 1);
         int x = xspace;
-        int y = TEXTURE_SIZE /2 ;
+        int y = TEXTURE_SIZE / 2;
 
-        
         BeginDrawing();
-            ClearBackground(BROWN);
+            ClearBackground(GROUND_COLOR);
             
             // Draw Cat Textures
             for (int i = 0 ; i < CAT_LENGTH; i++) {
-                DrawTexture(CAT_TEXTURES[i], x, y, WHITE);
-                x += xspace +TEXTURE_SIZE;
+                DrawTexture(CAT_PATURES[i].texture, x, y, WHITE);
+                x += xspace + TEXTURE_SIZE;
             }
 
-            // Draw Dice Button
+            // Draw Dice Texture
+            x = xspace;
+            y =  2 * TEXTURE_SIZE;
 
+            for(int i = 0 ; i < CAT_LENGTH && Dice[i];x += xspace + TEXTURE_SIZE , i++) {
+                DrawTexture(DICE_PATURES[Dice[i]].texture, x,y,WHITE);
+            }
+            
+            // Draw Dice Button
             int xbtn = (WINDOW_WIDTH - TEXTURE_SIZE) / 2;
             int ybtn = (4 * TEXTURE_SIZE) - CELL_SIZE;
 
-            DrawTexture(DICE_TEXTURES[0], xbtn , ybtn, WHITE);
-            DrawRectangleLines(
-                xbtn , ybtn ,
-                TEXTURE_SIZE , TEXTURE_SIZE ,
-                BLACK
-             );
-
-            // CheckCollision
-
-                x = xspace;
-                y =  2 * TEXTURE_SIZE;
-                Vector2 mouse = GetMousePosition();
-
-                // Draw Dice Textrue
-                    for(int i = 0 ; i < CAT_LENGTH && diceNumbers[i];x += xspace +TEXTURE_SIZE , i++){
-                        DrawTexture(DICE_TEXTURES[diceNumbers[i]], x,y,WHITE);
-                    }
-                
-
-                if(CheckCollisionPointRec(mouse , 
-                  (Rectangle){
-                   xbtn , ybtn , 
-                   TEXTURE_SIZE ,TEXTURE_SIZE}
-                ))
-                {
-
-                //If Click Button 
-                if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT)){
-                    int number = (rand() % 6) +1;
-
-                    if(!diceNumbers[index]){
-
-                        diceNumbers[index] = number;
-                        index = (index+1) % CAT_LENGTH;
-                    }
-                    else if(!SameNumber(diceNumbers,CAT_LENGTH,&i,&j) && diceNumbers[CAT_LENGTH-1]) PlayScreen();
-                    else{
-                        do
-                        {
-                        SameNumber(diceNumbers,CAT_LENGTH,&i,&j);
-                        number = (rand() % 6) +1;
-                        diceNumbers[i] = number;
-                        } while (SameNumber(diceNumbers,CAT_LENGTH,&i,&j));
-                        
-                    }
-                    
-                }
-             }
-
+            DrawTexture(
+                DICE_PATURES[0].texture,
+                xbtn, ybtn, WHITE
+            );
         EndDrawing();
+
+        
+        bool checkCollision = CheckCollisionPointRec(
+            GetMousePosition(), 
+            (Rectangle){
+                xbtn, ybtn,
+                TEXTURE_SIZE, TEXTURE_SIZE
+            }
+        );
+        
+        // Click Button 
+        if(!checkCollision || !IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
+            continue;
+
+        if (index < CAT_LENGTH) {
+            Dice[index] = rand() % (DICE_LENGTH - 1) + 1;
+            index ++;
+            continue;
+        }
+
+        int i = CheckRepeat();
+
+        if(i == CAT_LENGTH) exit(1);
+        while (CheckRepeat() <= i) {
+            Dice[i] = rand() % (DICE_LENGTH - 1) + 1;
+        }
     }
 }
 
@@ -144,5 +129,7 @@ void DiceScreen(void) {
 int main(void) {
     InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Mr. Clobber's Backyard");
 
+    srand(time(NULL));
+    
     DiceScreen();
 }
