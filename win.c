808d8d1dc29a8587b0a86210e760702cb065d8c6
ssaberifar <s.o.r.o.u.s.h.s.a.b.e.r.i.f.a.r@gmail.com>
2024-01-31 13:54:25 +0330

    temp

--- win.c
+++ win.c
@@ -9,27 +9,39 @@ Pature MEDALL_NAME[] = {
 // Win Screen
 void WinScreen() {
 
+    char *BtnLable[] = {
+        "Back to Menu"
+    };
    
     const int MEDALL_LENGTH = sizeof(MEDALL_NAME) / sizeof(char *);
+    const int BTNLABLE_LENGTH = sizeof(BtnLable) / sizeof(char *);
 
-    // Platform Rectangle
-    Rectangle Standrec[MEDALL_LENGTH] , rectangle={
+    char *StandNumber[MEDALL_LENGTH] ={
+        "2",
+        "1",
+        "3"
+    };
+
+    // Stand Rectangle
+    Rectangle StandOrButton[MEDALL_LENGTH ] , rectangle={
         WINDOW_DELTA + 3*CELL_SIZE,
         8 * CELL_SIZE,
         3 * CELL_SIZE,
         3 * CELL_SIZE
     };
 
-    //Intialize Standrec
+    // Intialize Standrec
     for(int i = 0 ; i < MEDALL_LENGTH ; i++){
-        Standrec[i] = rectangle;
-        Standrec[i].x += i * ( 3 *CELL_SIZE); 
+        StandOrButton[i] = rectangle;
+        StandOrButton[i].x += i * ( 3 *CELL_SIZE); 
     }
 
-    char *StandNumber[MEDALL_LENGTH] ={
-        "2",
-        "1",
-        "3"
+    // Intialize Button
+    Rectangle Button = { 
+        WINDOW_DELTA + 5 * CELL_SIZE ,
+        WINDOW_HEIGHT - (7/2) * CELL_SIZE ,
+        5 * CELL_SIZE,
+        3 * CELL_SIZE
     };
 
     //Intialize Line
@@ -45,6 +57,7 @@ void WinScreen() {
                 3 * CELL_SIZE
             );
 
+
     while(!WindowShouldClose()){
         BeginDrawing();
 
@@ -53,14 +66,14 @@ void WinScreen() {
 
             //Draw Standrec
             for(int i = 0 ; i < MEDALL_LENGTH ; i++){
-                DrawRectangleRec(Standrec[i],BORDER_COLOR);
+                DrawRectangleRec(StandOrButton[i],BORDER_COLOR);
 
                 //Draw Standrec Number
                 int textwidth = MeasureText(StandNumber[i], 2 *CELL_SIZE);
                 DrawText(
                         StandNumber[i],
-                        Standrec[i].x + (Standrec[i].width - textwidth) / 2,
-                        Standrec[i].y + (Standrec[i].height - CELL_SIZE) /2,
+                        StandOrButton[i].x + (StandOrButton[i].width - textwidth) / 2,
+                        StandOrButton[i].y + (StandOrButton[i].height - CELL_SIZE) /2,
                         2 * CELL_SIZE , WALL_COLOR
                 );
             }
@@ -68,12 +81,20 @@ void WinScreen() {
                 DrawLine(startx,y,endx,y , GROUND_COLOR);
 
             // Draw Medall Texture
-            int x = WINDOW_DELTA + CELL_SIZE; int y = CELL_SIZE;
+            int medallx = WINDOW_DELTA + CELL_SIZE; int y = CELL_SIZE;
             for(int i = 0  ; i < MEDALL_LENGTH ; i++ )
-                DrawTexture(MEDALL_NAME[i].texture, x += i * (3*CELL_SIZE) ,y , WHITE);
+                DrawTexture(MEDALL_NAME[i].texture, medallx += i * (3*CELL_SIZE) ,y , WHITE);
             
             // Draw Back To Menu Button
-            
+            DrawRectangleRec(Button,BORDER_COLOR);
+
+                //Draw Text 
+                int textwidth = MeasureText(BtnLable, 2 *CELL_SIZE);
+                DrawText(
+                    BtnLable[BTNLABLE_LENGTH],
+                    Button.x + (Button.width - textwidth) / 2,
+                    Button.y + Button.height - 
+                );
 
         EndDrawing();
     };
